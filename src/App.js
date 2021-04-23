ecdc0afd7b96816d6d2247c192c314d5bb9c1425
Sinda-BenMarzouk <sinda.benmarzouk@insat.u-carthage.tn>
2021-04-23 02:22:31 +0100

    products

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Home from './pages/home' ; 
 import Register from './pages/register' ; 
 import Login from './pages/login' ; 
+import Products from './pages/products' ; 
 import './App.css';
 import React ,  { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
@@ -15,6 +16,7 @@ class App extends Component{
              <Route path="/" component={Home} exact/>
              <Route path="/register" component={Register}/>
              <Route path="/login" component={Login}/>
+             <Route path="/products" component={Products}/>
            </Switch>
         </div> 
       </BrowserRouter>
